ad58a23f6e2188c2983df30620ec9d07db2ab711
Ritesh Pillai <7039911+ritesh404@users.noreply.github.com>
2024-11-13 22:00:45 +0530

    Update components/Authors.tsx
    Co-authored-by: irshadSprinto <irshada@sprinto.com>

--- components/Authors.tsx
+++ components/Authors.tsx
@@ -145,9 +145,7 @@ const Authors = () => {
             <button
               disabled={loading}
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={() => {
-                handleRefetchAuthors();
-              }}
+              onClick={handleRefetchAuthors}
             >
               Search
             </button>
