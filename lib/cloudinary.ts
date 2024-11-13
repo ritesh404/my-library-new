6e845b3201bfb7a6e0a636e429913fd6e38b61ec
Ritesh <riteshp@sprinto.com>
2024-11-13 23:16:40 +0530

    fixes

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -1,27 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export const uploadToCloudinary = async (file: File): Promise<string> => {
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append(
-    "upload_preset",
-    process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET!
-  );
-  formData.append("folder", "books");
+  const buffer = await file.arrayBuffer().then((ab) => Buffer.from(ab));
 
   try {
-    const response = await fetch(
-      `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
-      {
-        method: "POST",
-        body: formData,
+    const response: { secure_url: string } | undefined = await new Promise(
+      (resolve) => {
+        cloudinary.uploader
+          .upload_stream((error, uploadResult) => {
+            return resolve(uploadResult);
+          })
+          .end(buffer);
       }
     );
 
-    const data = await response.json();
-    if (!response.ok) {
-      throw new Error(data.error?.message || "Failed to upload image");
+    if (!response) {
+      throw new Error("No response from Cloudinary");
     }
-
-    return data.secure_url;
+    return response.secure_url;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error);
     throw error;
